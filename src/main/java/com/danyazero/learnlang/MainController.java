ffb7f7a8a60ce024f6e37dea74a1020d033431b4
danyazero <formulamgo2@gmail.com>
2023-03-26 15:50:12 +0300

    Initial commit

---
+++ src/main/java/com/danyazero/learnlang/MainController.java
@@ -0,0 +1,14 @@
+package com.danyazero.learnlang;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to Zero lang Application!");
+    }
+}
---
+++ src/main/java/com/danyazero/learnlang/MyApplication.java
@@ -0,0 +1,23 @@
+package com.danyazero.learnlang;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MyApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MyApplication.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Zero lang");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
