2a925fc5bb08069aade3f6197f0e39de8d894bb4
danyazero <formulamgo2@gmail.com>
2023-03-27 16:33:29 +0300

    navigate between words & show answer

--- src/main/java/com/danyazero/learnlang/MainController.java
+++ src/main/java/com/danyazero/learnlang/MainController.java
@@ -3,10 +3,8 @@ package com.danyazero.learnlang;
 import IO.IOProcessor;
 import IO.IO;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
-import javafx.scene.layout.HBox;
 import models.FileOutput;
 
 import java.util.List;
@@ -15,13 +13,17 @@ public class MainController {
     public MenuBar menuBar;
     public Label bottomText;
     public Label topText;
+    public Label idLabel;
 
     private int id = 0;
-    private IO io;
+    private IO io = new IOProcessor();
     private List<FileOutput> list;
 
     public void initialize(){
         list = io.readObjectsFromJSON();
+        topText.setText(list.get(id).getTranslate());
+        bottomText.setText(getEnglishTranslate(list));
+        idLabel.setText(String.valueOf(id));
     }
 
     public void instagramLink(ActionEvent actionEvent) {
@@ -36,6 +38,30 @@ public class MainController {
     }
 
     public void nextButtonHandler(ActionEvent actionEvent) {
-        id++;
+        if (id < list.size()-1){
+            id++;
+            renderData();
+        }
+    }
+
+    private void renderData(){
+        bottomText.setVisible(false);
+        topText.setText(list.get(id).getTranslate());
+        bottomText.setText(getEnglishTranslate(list));
+        idLabel.setText(String.valueOf(id));
+    }
+
+    private String getEnglishTranslate(List<FileOutput> words) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(words.get(id).getFirstForm()).append(", ").append(words.get(id).getSecondForm()).append(", ").append(words.get(id).getThirdForm());
+
+        return sb.toString();
+    }
+
+    public void previousButtonHandler(ActionEvent actionEvent) {
+        if (id > 0){
+            id--;
+            renderData();
+        }
     }
 }
--- src/main/java/com/danyazero/learnlang/MyApplication.java
+++ src/main/java/com/danyazero/learnlang/MyApplication.java
@@ -3,6 +3,7 @@ package com.danyazero.learnlang;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class MyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Zero lang");
         stage.setScene(scene);
         stage.setResizable(false);
